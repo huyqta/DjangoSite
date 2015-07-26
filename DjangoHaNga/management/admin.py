0c6c926f5dfc2843f69417475c24f9de3cc1635e
Huy Quan <anhhuydng@gmail.com>
2015-07-27 00:39:31 +0700

    Update change

--- DjangoHaNga/management/admin.py
+++ DjangoHaNga/management/admin.py
@@ -1,31 +1,52 @@
 from django.contrib import admin
 
-from management.models import Category, Product, Service, Project
+from management.models import Category, Product
 
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["cat_name", "description"]
+    fields = ["cat_name", "description"]
+
+   
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["product_name", "product_title", "release_date", "host_address", "domain_address", "introduction",
-                    "product_type", "active"]
-    fields = ["product_name", "product_title", "release_date", "host_address", "domain_address", "introduction",
-              "product_type", "active"]
+    list_display = ["product_name", "get_category", "description"]
+    fields = ["product_name", "get_category", "description"]
+     
+    def get_category(self, obj):
+        return obj.cat_parent.cat_name
+
+#class ProductAdmin(admin.ModelAdmin):
+#    list_display = ["product_name", "product_title", "release_date",
+#    "host_address", "domain_address", "introduction",
+#                    "product_type", "active"]
+#    fields = ["product_name", "product_title", "release_date", "host_address",
+#    "domain_address", "introduction",
+#              "product_type", "active"]
 
 
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ["Name", "Code", "Description", "HostAddress", "DomainAddress", "Active",
-                    "ReleaseDate"]
-    fields = ["Name", "Code", "Description", "HostAddress", "DomainAddress", "Active",
-                    "ReleaseDate"]
+#class ServiceAdmin(admin.ModelAdmin):
+#    list_display = ["Name", "Code", "Description", "HostAddress",
+#    "DomainAddress", "Active",
+#                    "ReleaseDate"]
+#    fields = ["Name", "Code", "Description", "HostAddress", "DomainAddress",
+#    "Active",
+#                    "ReleaseDate"]
 
 
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ["Name", "Code", "Description", "ImageUrl", "ThumbImageUrl", "ReleaseUrl",
-                    "TeamSize", "StartDate", "CloseDate", "Status", "Active", "ReleaseDate"]
-    fields = ["Name", "Code", "Description", "ImageUrl", "ThumbImageUrl", "ReleaseUrl",
-                    "TeamSize", "StartDate", "CloseDate", "Status", "Active", "ReleaseDate"]
+#class ProjectAdmin(admin.ModelAdmin):
+#    list_display = ["Name", "Code", "Description", "ImageUrl",
+#    "ThumbImageUrl", "ReleaseUrl",
+#                    "TeamSize", "StartDate", "CloseDate", "Status", "Active",
+#                    "ReleaseDate"]
+#    fields = ["Name", "Code", "Description", "ImageUrl", "ThumbImageUrl",
+#    "ReleaseUrl",
+#                    "TeamSize", "StartDate", "CloseDate", "Status", "Active",
+#                    "ReleaseDate"]
 
-
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Service, ServiceAdmin)
-admin.site.register(Project, ProjectAdmin)
+#admin.site.register(Service, ServiceAdmin)
+#admin.site.register(Project, ProjectAdmin)
 
--- DjangoHaNga/management/models.py
+++ DjangoHaNga/management/models.py
@@ -1,31 +1,44 @@
+import uuid
 from django.db import models
-
 from management import enums
 
 
 # Create your models here.
 class Category(models.Model):
-    code = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    cat_name = models.CharField(max_length=255)    
+    cat_parent = models.ForeignKey('self',default=uuid.uuid4, editable=False, unique=True)
+    description = models.CharField(max_length=255, blank=True)
 
+    def __unicode__(self):
+        return self.cat_name
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=500, verbose_name="Product name")
-    product_title = models.CharField(max_length=500, verbose_name="Product title")
-    release_date = models.DateField(verbose_name="Release date")
-    host_address = models.CharField(max_length=500, verbose_name="Host", blank=True)
-    domain_address = models.CharField(max_length=500, verbose_name="Domain", blank=True)
-    introduction = models.CharField(max_length=500, verbose_name="Introduction", blank=True)
-    product_type = models.CharField(max_length=255, choices=enums.ListEnums.PRODUCT_TYPES, verbose_name="Type")
-    active = models.BooleanField(verbose_name="Active")
-
-    class Meta:
-        ordering = ["release_date"]
-        pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product_name = models.CharField(max_length=255)
+    category_id = models.ForeignKey(Category)
+    description = models.CharField(max_length=255, blank=True)
 
     def __unicode__(self):
         return self.product_name
+
+
+#class Product(models.Model):
+#    product_name = models.CharField(max_length=500, verbose_name="Product name")
+#    product_title = models.CharField(max_length=500, verbose_name="Product title")
+#    release_date = models.DateField(verbose_name="Release date")
+#    host_address = models.CharField(max_length=500, verbose_name="Host", blank=True)
+#    domain_address = models.CharField(max_length=500, verbose_name="Domain", blank=True)
+#    introduction = models.CharField(max_length=500, verbose_name="Introduction", blank=True)
+#    product_type = models.CharField(max_length=255, choices=enums.ListEnums.PRODUCT_TYPES, verbose_name="Type")
+#    active = models.BooleanField(verbose_name="Active")
+
+#    class Meta:
+#        ordering = ["release_date"]
+#        pass
+
+#    def __unicode__(self):
+#        return self.product_name
 
 
 class Service(models.Model):
@@ -38,9 +51,9 @@ class Service(models.Model):
     Active = models.BooleanField(verbose_name="Active service")
     ReleaseDate = models.DateTimeField(verbose_name="Release date")
 
-    class Meta:
-        ordering = ["Active", "Name", "ReleaseDate"]
-        pass
+#    class Meta:
+#        ordering = ["Active", "Name", "ReleaseDate"]
+#        pass
 
 
 class Project(models.Model):
@@ -58,5 +71,5 @@ class Project(models.Model):
     Active = models.BooleanField(verbose_name="Active service")
     ReleaseDate = models.DateTimeField(verbose_name="Release date")
 
-    class Meta:
-        ordering = ["Active", "Name", "ReleaseDate"]
+    #class Meta:
+    #    ordering = ["Active", "Name", "ReleaseDate"]
